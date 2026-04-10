de8f367eb4316bcd57e032100cbcaa38560ced55
antocastillo <ascastillo@escuelasproa.edu.ar>
2026-04-09 21:36:42 -0300

    ejercicio2

--- trabajo.py
+++ trabajo.py
@@ -15,11 +15,42 @@ def evaluar_compra(total, nombre, comida):
 
     return f"{nombre} compró {comida}: {estado}"
 
+def clasificar_total(total):
+    if total == 0:
+        return "Sin compra"
+    elif total < 3000:
+        return "Compra baja"
+    else:
+        return "Compra alta"
+
+def mostrar_productos(lista_precios):
+    print("Productos comprados:")
+    for i in range(len(lista_precios)):
+        print(f"Producto {i+1}: ${lista_precios[i]}")
+
+def contar_productos(lista_precios):
+    i = 0
+    contador = 0
+    while i < len(lista_precios):
+        contador += 1
+        i += 1
+    return contador
+
 def mostrar_ticket(precio1, precio2, precio3):
+    precios = [precio1, precio2, precio3]
+    
     total = calcular_precio_total(precio1, precio2, precio3)
     mensaje = evaluar_compra(total, "Anto", "hamburguesas")
+    
+    clasificacion = clasificar_total(total)
+    cantidad = contar_productos(precios)
+
     print("Total a pagar: $", total)
     print(mensaje)
-
+    print("Clasificación:", clasificacion)
+    print("Cantidad de productos:", cantidad)
+    mostrar_productos(precios)
 
 mostrar_ticket(2000, 1500, 1800)
+
+    
